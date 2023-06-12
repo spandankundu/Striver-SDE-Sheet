84f3f2741e2778bcf54739f0fc6e615cdc9aecce
spandankundu <99378538+spandankundu@users.noreply.github.com>
2023-06-12 10:05:13 +0530

    Update Q14.cpp

--- Q14.cpp
+++ Q14.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
-int modularExponentiation(int x, int n, int m) {
+#define ll long long
+ll modularExponentiation(ll x, ll n, ll m) {
 	if(n==0) return 1;
-	int res=modularExponentiation(x,n/2,m);
+	ll res=modularExponentiation(x,n/2,m);
 	if(n%2==1) return (res%m*res%m*x%m)%m;
 	else return (res%m*res%m)%m;
 }
